0187831aca96c85d04071f36ebea0b3d2f24dce8
relliebalagat <relliebalagat@gmail.com>
2018-08-22 09:48:53 +0800

    new sql file

--- assets/config/amanentez.sql
+++ assets/config/amanentez.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 29, 2018 at 04:07 PM
+-- Generation Time: Aug 22, 2018 at 03:46 AM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
@@ -113,7 +113,27 @@ CREATE TABLE `booking_rooms` (
 
 INSERT INTO `booking_rooms` (`reservation_id`, `room_id`) VALUES
 (1, 1),
-(2, 1);
+(2, 1),
+(3, 1),
+(3, 1),
+(3, 2),
+(3, 2),
+(3, 3),
+(3, 3),
+(4, 1),
+(4, 2),
+(5, 1),
+(5, 2),
+(6, 1),
+(6, 4),
+(7, 6),
+(8, 3),
+(9, 1),
+(11, 1),
+(12, 3),
+(12, 3),
+(13, 1),
+(13, 1);
 
 -- --------------------------------------------------------
 
@@ -138,7 +158,18 @@ CREATE TABLE `client` (
 
 INSERT INTO `client` (`id`, `first_name`, `last_name`, `contact_number`, `email`, `client_address`, `birthday`, `date_registered`) VALUES
 (1, 'test', 'test', '1', 'test@gmail.com', 'test', '2001-01-01', '2018-07-29 10:02:26'),
-(2, 'test2', 'test2', '1234567', 'test2@gmail.com', 'test2', '2001-01-01', '2018-07-29 10:06:07');
+(2, 'test2', 'test2', '1234567', 'test2@gmail.com', 'test2', '2001-01-01', '2018-07-29 10:06:07'),
+(3, 'test', 'test', '123', 'test@test.com', 'test', '2991-01-01', '2018-08-01 00:21:07'),
+(4, 'rellie', 'balagat', '123', 'test', 'test', '2001-01-01', '2018-08-04 05:46:11'),
+(5, 'rellie', 'balagat', '123', 'test', 'test', '2001-01-01', '2018-08-04 05:46:53'),
+(6, 'test', 'test', '123', 'test', 'test', '2001-01-01', '2018-08-04 05:48:18'),
+(7, 'test', 'test', '123', 'test', 'test', '2001-01-01', '2018-08-04 05:51:10'),
+(8, 'qwe', 'qwe', '1234567', 'qwe@gmail.com', 'qwe', '2001-01-01', '2018-08-04 06:13:31'),
+(9, 'leand', 'viray', '9983268404', 'virayleand@gmail.com', 'taytayrizal', '1999-12-19', '2018-08-04 08:40:44'),
+(10, 'Rellie', 'Balagat', '1', 'relliebalagat@gmail.com', 'test', '2003-01-01', '2018-08-05 22:49:43'),
+(11, 'Rellie', 'Balagat', '1234567', 'rellierubiobalagat@gmail.com', 'e', '2001-01-01', '2018-08-18 12:39:35'),
+(12, 'Botnog', 'Balagat', '1234567', 'botnog@gmail.com', '86-A Tandang Sora QC', '0200-01-01', '2018-08-19 03:50:11'),
+(13, 'rel', 'bal', '1234567', 'rel@gmail.com', '86-A Tandang Sora QC', '2001-01-01', '2018-08-19 04:06:48');
 
 -- --------------------------------------------------------
 
@@ -164,6 +195,36 @@ CREATE TABLE `expenses` (
   `amount` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `expenses`
+--
+
+INSERT INTO `expenses` (`id`, `name`, `amount`) VALUES
+(1, 'Extra Person (Bed, Linen and Towel)', '600.00'),
+(2, 'Extra Person with Towel (Adult or Children)', '300.00'),
+(3, 'Extra Towel', '50.00'),
+(4, 'Extra Pillow', '50.00'),
+(5, 'Extra Comforter', '150.00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `file_reservation`
+--
+
+CREATE TABLE `file_reservation` (
+  `reservation_id` int(11) NOT NULL,
+  `file_name` varchar(100) NOT NULL,
+  `date_created` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `file_reservation`
+--
+
+INSERT INTO `file_reservation` (`reservation_id`, `file_name`, `date_created`) VALUES
+(13, 'payment_photo/38996989_229907571042362_6981769922934210560_n.jpg', '0000-00-00 00:00:00');
+
 -- --------------------------------------------------------
 
 --
@@ -178,6 +239,7 @@ CREATE TABLE `reservation` (
   `client_id` int(11) NOT NULL,
   `person_count` int(11) NOT NULL,
   `type` varchar(50) DEFAULT NULL,
+  `payment` varchar(50) NOT NULL,
   `status` varchar(50) NOT NULL,
   `date_created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `date_updated` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
@@ -187,9 +249,20 @@ CREATE TABLE `reservation` (
 -- Dumping data for table `reservation`
 --
 
-INSERT INTO `reservation` (`id`, `reference_no`, `check_in`, `check_out`, `client_id`, `person_count`, `type`, `status`, `date_created`, `date_updated`) VALUES
-(1, 'AMNZ-06A67A5', '2018-07-30', '2018-07-31', 1, 1, 'Walk In', '', '2018-07-29 10:02:26', '2018-07-29 10:02:26'),
-(2, 'AMNZ-8D80B58', '2018-07-30', '2018-07-31', 2, 1, 'Walk In', '', '2018-07-29 10:06:07', '2018-07-29 10:06:07');
+INSERT INTO `reservation` (`id`, `reference_no`, `check_in`, `check_out`, `client_id`, `person_count`, `type`, `payment`, `status`, `date_created`, `date_updated`) VALUES
+(1, 'AMNZ-06A67A5', '2018-07-30', '2018-07-31', 1, 1, 'Walk In', '', '', '2018-07-29 10:02:26', '2018-07-29 10:02:26'),
+(2, 'AMNZ-8D80B58', '2018-07-30', '2018-07-31', 2, 1, 'Walk In', '', '', '2018-07-29 10:06:07', '2018-07-29 10:06:07'),
+(3, 'AMNZ-FFA084A', '2018-08-02', '2018-08-03', 3, 3, 'Walk In', '', '', '2018-08-01 00:21:07', '2018-08-01 00:21:07'),
+(4, 'AMNZ-0A5544B', '2018-08-05', '2018-08-06', 4, 1, 'Online Reservation', '', 'FOR PAYMENT', '2018-08-04 05:46:12', '2018-08-04 05:46:12'),
+(5, 'AMNZ-A9A25F3', '2018-08-05', '2018-08-06', 5, 1, 'Online Reservation', '', 'FOR PAYMENT', '2018-08-04 05:46:53', '2018-08-04 05:46:53'),
+(6, 'AMNZ-702D8B4', '2018-08-05', '2018-08-06', 6, 1, 'Online Reservation', '', 'FOR PAYMENT', '2018-08-04 05:48:18', '2018-08-04 05:48:18'),
+(7, 'AMNZ-13EF3F9', '0000-00-00', '0000-00-00', 7, 1, 'Online Reservation', '', 'FOR PAYMENT CONFIRMATION', '2018-08-04 05:51:10', '2018-08-04 05:55:33'),
+(8, 'AMNZ-A265D04', '0000-00-00', '0000-00-00', 8, 1, 'Online Reservation', '', 'FOR PAYMENT', '2018-08-04 06:13:31', '2018-08-04 06:14:32'),
+(9, 'AMNZ-FA893B0', '2018-08-17', '2018-08-18', 9, 1, 'Online Reservation', '', 'FOR PAYMENT CONFIRMATION', '2018-08-04 08:40:45', '2018-08-04 08:45:25'),
+(10, 'AMNZ-A5A7C0B', '2018-08-07', '2018-08-08', 10, 2, 'Online Reservation', '', 'FOR PAYMENT', '2018-08-05 22:49:43', '2018-08-05 22:49:43'),
+(11, 'AMNZ-C68BCB3', '1970-01-01', '1970-01-01', 11, 0, 'Online Reservation', '', 'FOR PAYMENT', '2018-08-18 12:39:36', '2018-08-18 12:39:36'),
+(12, 'AMNZ-74E55F8', '1970-01-01', '1970-01-01', 12, 0, 'Online Reservation', '', 'FOR PAYMENT CONFIRMATION', '2018-08-19 03:50:11', '2018-08-19 23:23:57'),
+(13, 'AMNZ-9F1901C', '2018-09-02', '2018-09-08', 13, 4, 'Online Reservation', '', 'FOR PAYMENT CONFIRMATION', '2018-08-19 04:06:48', '2018-08-21 23:56:48');
 
 -- --------------------------------------------------------
 
@@ -216,10 +289,7 @@ INSERT INTO `room` (`id`, `type`, `description`, `simple_description`, `capacity
 (3, 'Deluxe', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', ' Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.', 2, '3200.00'),
 (4, 'Superior Room', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', ' Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.', 2, '3500.00'),
 (5, 'Executive Room', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 4, '4200.00'),
-(6, 'VIP Beach Front', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 4, '4900.00'),
-(7, 'room1', 'test', 'test', 1, '1.00'),
-(8, 'room1', 'test', 'test', 1, '1.00'),
-(9, 'room1', 'test', 'test', 1, '1.00');
+(6, 'VIP Beach Front', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 4, '4900.00');
 
 -- --------------------------------------------------------
 
@@ -238,7 +308,7 @@ CREATE TABLE `room_status` (
 --
 
 INSERT INTO `room_status` (`id`, `room_id`, `status_id`) VALUES
-(1, 6, 1),
+(1, 6, 3),
 (2, 5, 1),
 (3, 3, 1),
 (4, 5, 1),
@@ -379,6 +449,12 @@ ALTER TABLE `expenses`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `file_reservation`
+--
+ALTER TABLE `file_reservation`
+  ADD KEY `reservation_id` (`reservation_id`);
+
+--
 -- Indexes for table `reservation`
 --
 ALTER TABLE `reservation`
@@ -432,7 +508,7 @@ ALTER TABLE `account`
 -- AUTO_INCREMENT for table `client`
 --
 ALTER TABLE `client`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `discount`
@@ -444,13 +520,13 @@ ALTER TABLE `discount`
 -- AUTO_INCREMENT for table `reservation`
 --
 ALTER TABLE `reservation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `room`
 --
 ALTER TABLE `room`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `room_status`
@@ -506,6 +582,12 @@ ALTER TABLE `booking_expenses`
 ALTER TABLE `booking_rooms`
   ADD CONSTRAINT `booking_rooms_ibfk_1` FOREIGN KEY (`reservation_id`) REFERENCES `reservation` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `booking_rooms_ibfk_2` FOREIGN KEY (`room_id`) REFERENCES `room` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `file_reservation`
+--
+ALTER TABLE `file_reservation`
+  ADD CONSTRAINT `file_reservation_ibfk_1` FOREIGN KEY (`reservation_id`) REFERENCES `reservation` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `reservation`
